caef4096d31564c88fcdf42a9853946052f4b5cf
ro14081998 <61298369+ro14081998@users.noreply.github.com>
2021-10-13 16:38:14 -0300

    criacao pasta database
    codigo da base para teste

--- config/config.php
+++ config/config.php
@@ -1,4 +1,9 @@
 <?php
 #configuracoes do site
-
+#conexao ao banco de dados
+$data_connection = 'mysql:dbname=testeAPI;host=127.0.0.1';
+$user ='root';
+$password='';
+$connection = new PDO($data_connection,$user,$password);
+echo "Conexão bem sucedida!";
 ?>
